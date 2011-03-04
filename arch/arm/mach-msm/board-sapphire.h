dazed-nerd/kernel-2635

Update 32A/EBI1 patch from Bcrook and Pershoot.

arch/arm/mach-msm/board-sapphire.h
@@ -19,53 +19,98 @@
 #define MSM_SMI_BASE		0x00000000
 #define MSM_SMI_SIZE		0x00800000
 
-#define MSM_EBI_BASE		0x10000000
-#define MSM_EBI_SIZE		0x06e00000
-
 #define MSM_PMEM_GPU0_BASE	0x00000000
 #define MSM_PMEM_GPU0_SIZE	0x00700000
 
-#define SMI64_MSM_PMEM_MDP_BASE	0x02000000
-#define SMI64_MSM_PMEM_MDP_SIZE	0x00800000
+#if defined(CONFIG_MSM_AMSS_SUPPORT_256MB_EBI1)
+/* AMSS supports unified 128/256MB EBI1 */
+#define MSM_EBI_BASE                            0x19200000
+#define MSM_EBI_SMI64_128MB_SIZE        0x06600000
+#define MSM_EBI_SMI32_256MB_SIZE        0x0C600000
 
-#define SMI64_MSM_PMEM_ADSP_BASE      0x02800000
+#define SMI64_MSM_FB_BASE                       0x00700000
+#define SMI64_MSM_FB_SIZE                       0x0009B000
+
+#define SMI64_MSM_RAM_CONSOLE_BASE      0x007A0000
+#define SMI64_MSM_RAM_CONSOLE_SIZE      128 * SZ_1K
+
+#define SMI64_MSM_PMEM_MDP_BASE         0x02000000
+#define SMI64_MSM_PMEM_MDP_SIZE         0x00800000
+
+#define SMI64_MSM_PMEM_ADSP_BASE    0x02800000
+
 #define SMI64_MSM_PMEM_ADSP_SIZE	0x00800000
 
 #define SMI64_MSM_PMEM_CAMERA_BASE	0x03000000
 #define SMI64_MSM_PMEM_CAMERA_SIZE	0x01000000
 
-#define SMI64_MSM_FB_BASE		0x00700000
-#define SMI64_MSM_FB_SIZE		0x00100000
+#define SMI64_MSM_PMEM_GPU1_BASE        0x1F800000
+#define SMI64_MSM_PMEM_GPU1_SIZE        0x800000
+
+#define SMI32_MSM_FB_BASE                       0x00700000
+#define SMI32_MSM_FB_SIZE                       0x0009B000
+
+#define SMI32_MSM_RAM_CONSOLE_BASE      0x007A0000
+#define SMI32_MSM_RAM_CONSOLE_SIZE      128 * SZ_1K
+
+#define SMI32_MSM_PMEM_GPU1_BASE        0x25800000
+#define SMI32_MSM_PMEM_GPU1_SIZE        0x00800000
+
+#define SMI32_MSM_PMEM_MDP_BASE         0x26000000
+#define SMI32_MSM_PMEM_MDP_SIZE         0x00800000
+
+#define SMI32_MSM_PMEM_ADSP_BASE        0x26800000
+#define SMI32_MSM_PMEM_ADSP_SIZE        0x00800000
+
+#define SMI32_MSM_PMEM_CAMERA_BASE      0x27000000
+#define SMI32_MSM_PMEM_CAMERA_SIZE      0x01000000
+
+#else
+/* AMSS supports only 128MB EBI1. */
+#define MSM_EBI_BASE                            0x10000000
+
+#define SMI64_MSM_FB_BASE                       0x00700000
+#define SMI64_MSM_FB_SIZE                       0x00100000
+
+#define SMI64_MSM_PMEM_MDP_BASE         0x02000000
+#define SMI64_MSM_PMEM_MDP_SIZE         0x00800000
+
+#define SMI64_MSM_PMEM_ADSP_BASE    0x02800000
+#define SMI64_MSM_PMEM_ADSP_SIZE        0x00800000
+
+#define SMI64_MSM_PMEM_CAMERA_BASE      0x03000000
+#define SMI64_MSM_PMEM_CAMERA_SIZE      0x01000000
 
 #define SMI64_MSM_LINUX_BASE		MSM_EBI_BASE
 #define SMI64_MSM_LINUX_SIZE		0x06500000
 
+#define SMI64_MSM_RAM_CONSOLE_BASE      MSM_EBI_BASE + 0x06D00000
+#define SMI64_MSM_RAM_CONSOLE_SIZE      128 * SZ_1K
+
+#define SMI64_MSM_PMEM_GPU1_SIZE        0x800000
+#define SMI64_MSM_PMEM_GPU1_BASE        (SMI64_MSM_RAM_CONSOLE_BASE - SMI64_MSM_PMEM_GPU1_SIZE)
 
 #define SMI32_MSM_LINUX_BASE		MSM_EBI_BASE
 #define SMI32_MSM_LINUX_SIZE		0x5400000
 
-#define SMI32_MSM_PMEM_MDP_BASE	SMI32_MSM_LINUX_BASE + SMI32_MSM_LINUX_SIZE
-#define SMI32_MSM_PMEM_MDP_SIZE	0x800000
+#define SMI32_MSM_PMEM_MDP_BASE    (SMI32_MSM_LINUX_BASE + SMI32_MSM_LINUX_SIZE)
+#define SMI32_MSM_PMEM_MDP_SIZE    0x800000
 
-#define SMI32_MSM_PMEM_ADSP_BASE	SMI32_MSM_PMEM_MDP_BASE + SMI32_MSM_PMEM_MDP_SIZE
+#define SMI32_MSM_PMEM_ADSP_BASE  (SMI32_MSM_PMEM_MDP_BASE + SMI32_MSM_PMEM_MDP_SIZE)
 #define SMI32_MSM_PMEM_ADSP_SIZE	0x800000
 
-#define SMI32_MSM_FB_BASE		SMI32_MSM_PMEM_ADSP_BASE + SMI32_MSM_PMEM_ADSP_SIZE
-#define SMI32_MSM_FB_SIZE		0x9b000
+#define SMI32_MSM_FB_BASE      (SMI32_MSM_PMEM_ADSP_BASE + SMI32_MSM_PMEM_ADSP_SIZE)
+#define SMI32_MSM_FB_SIZE      0x9b000
 
+#define SMI32_MSM_RAM_CONSOLE_BASE  (MSM_EBI_BASE + 0x6d00000)
+#define SMI32_MSM_RAM_CONSOLE_SIZE  128 * SZ_1K
 
-#define MSM_PMEM_GPU1_SIZE	0x800000
-#define MSM_PMEM_GPU1_BASE	MSM_RAM_CONSOLE_BASE - MSM_PMEM_GPU1_SIZE
+#define SMI32_MSM_PMEM_GPU1_SIZE  0x800000
+#define SMI32_MSM_PMEM_GPU1_BASE  (SMI32_MSM_RAM_CONSOLE_BASE - SMI32_MSM_PMEM_GPU1_SIZE)
 
-#define MSM_RAM_CONSOLE_BASE	MSM_EBI_BASE + 0x6d00000
-#define MSM_RAM_CONSOLE_SIZE	128 * SZ_1K
+#define SMI32_MSM_PMEM_CAMERA_BASE  0
+#define SMI32_MSM_PMEM_CAMERA_SIZE  0
 
-#if (SMI32_MSM_FB_BASE + SMI32_MSM_FB_SIZE) >= (MSM_PMEM_GPU1_BASE)
-#error invalid memory map
-#endif
-
-#if (SMI64_MSM_FB_BASE + SMI64_MSM_FB_SIZE) >= (MSM_PMEM_GPU1_BASE)
-#error invalid memory map
 #endif
 
 #define EBI1_DUAL_128MB_128MB	17

sileht/cm-kernel

Change-Id: Icc3fefa076643b44643d5c4be34d52b46a3a1df9

arch/arm/mach-msm/board-sapphire.h
@@ -19,6 +19,54 @@
 #define MSM_SMI_BASE		0x00000000
 #define MSM_SMI_SIZE		0x00800000
 
+#define MSM_PMEM_GPU0_BASE	0x00000000
+#define MSM_PMEM_GPU0_SIZE	0x00700000
+
+#if defined(CONFIG_MSM_AMSS_SUPPORT_256MB_EBI1)
+/* AMSS supports unified 128/256MB EBI1 */
+#define MSM_EBI_BASE                            0x19200000
+#define MSM_EBI_SMI64_128MB_SIZE        0x06600000
+#define MSM_EBI_SMI32_256MB_SIZE        0x0C600000
+
+#define SMI64_MSM_FB_BASE                       0x00700000
+#define SMI64_MSM_FB_SIZE                       0x0009B000
+
+#define SMI64_MSM_RAM_CONSOLE_BASE      0x007A0000
+#define SMI64_MSM_RAM_CONSOLE_SIZE      128 * SZ_1K
+
+#define SMI64_MSM_PMEM_MDP_BASE         0x02000000
+#define SMI64_MSM_PMEM_MDP_SIZE         0x00800000
+
+#define SMI64_MSM_PMEM_ADSP_BASE    0x02800000
+
+#define SMI64_MSM_PMEM_ADSP_SIZE	0x00800000
+
+#define SMI64_MSM_PMEM_CAMERA_BASE	0x15400000
+#define SMI64_MSM_PMEM_CAMERA_SIZE	0x00500000
+
+#define SMI64_MSM_PMEM_GPU1_BASE        0x1F800000
+#define SMI64_MSM_PMEM_GPU1_SIZE        0x800000
+
+#define SMI32_MSM_FB_BASE                       0x00700000
+#define SMI32_MSM_FB_SIZE                       0x0009B000
+
+#define SMI32_MSM_RAM_CONSOLE_BASE      0x007A0000
+#define SMI32_MSM_RAM_CONSOLE_SIZE      128 * SZ_1K
+
+#define SMI32_MSM_PMEM_GPU1_BASE        0x25800000
+#define SMI32_MSM_PMEM_GPU1_SIZE        0x00800000
+
+#define SMI32_MSM_PMEM_MDP_BASE         0x26000000
+#define SMI32_MSM_PMEM_MDP_SIZE         0x00800000
+
+#define SMI32_MSM_PMEM_ADSP_BASE        0x26800000
+#define SMI32_MSM_PMEM_ADSP_SIZE        0x00800000
+
+#define SMI32_MSM_PMEM_CAMERA_BASE      0x27000000
+#define SMI32_MSM_PMEM_CAMERA_SIZE      0x01000000
+
+#else
+/* AMSS supports only 128MB EBI1. */
 #define MSM_EBI_BASE		0x10000000
 #define MSM_EBI_SIZE		0x07100000
 
@@ -29,13 +77,13 @@
 #define SMI64_MSM_PMEM_MDP_SIZE	0x00800000
 
 #define SMI64_MSM_PMEM_ADSP_BASE	0x16100000
-#define SMI64_MSM_PMEM_ADSP_SIZE	0x00800000
+#define SMI64_MSM_PMEM_ADSP_SIZE    0x00800000
 
-#define SMI64_MSM_PMEM_CAMERA_BASE	0x15400000
-#define SMI64_MSM_PMEM_CAMERA_SIZE	0x00500000
+#define SMI64_MSM_PMEM_CAMERA_BASE  0x15400000
+#define SMI64_MSM_PMEM_CAMERA_SIZE  0x00500000
 
-#define SMI64_MSM_FB_BASE		0x00700000
-#define SMI64_MSM_FB_SIZE		0x00100000
+#define SMI64_MSM_FB_BASE           0x00700000
+#define SMI64_MSM_FB_SIZE           0x00100000
 
 #define SMI64_MSM_LINUX_BASE		MSM_EBI_BASE
 #define SMI64_MSM_LINUX_SIZE		0x068e0000
@@ -46,31 +94,34 @@
 #define SMI64_MSM_LINUX_BASE_2		MSM_EBI_BASE
 #define SMI64_MSM_LINUX_SIZE_2		0x05400000
 
+#define SMI64_MSM_RAM_CONSOLE_BASE	0x169E0000
+#define SMI64_MSM_RAM_CONSOLE_SIZE	128 * SZ_1K
+
+#define SMI64_MSM_PMEM_GPU1_SIZE	0x800000
+#define SMI64_MSM_PMEM_GPU1_BASE     (SMI64_MSM_RAM_CONSOLE_BASE + SMI64_MSM_RAM_CONSOLE_SIZE)
+
 #define SMI32_MSM_LINUX_BASE		MSM_EBI_BASE
 #define SMI32_MSM_LINUX_SIZE		0x5400000
 
-#define SMI32_MSM_PMEM_MDP_BASE	SMI32_MSM_LINUX_BASE + SMI32_MSM_LINUX_SIZE
-#define SMI32_MSM_PMEM_MDP_SIZE	0x800000
+#define SMI32_MSM_PMEM_MDP_BASE    (SMI32_MSM_LINUX_BASE + SMI32_MSM_LINUX_SIZE)
+#define SMI32_MSM_PMEM_MDP_SIZE    0x800000
 
-#define SMI32_MSM_PMEM_ADSP_BASE	SMI32_MSM_PMEM_MDP_BASE + SMI32_MSM_PMEM_MDP_SIZE
+#define SMI32_MSM_PMEM_ADSP_BASE  (SMI32_MSM_PMEM_MDP_BASE + SMI32_MSM_PMEM_MDP_SIZE)
 #define SMI32_MSM_PMEM_ADSP_SIZE	0x800000
 
-#define SMI32_MSM_FB_BASE		SMI32_MSM_PMEM_ADSP_BASE + SMI32_MSM_PMEM_ADSP_SIZE
-#define SMI32_MSM_FB_SIZE		0x9b000
+#define SMI32_MSM_FB_BASE      (SMI32_MSM_PMEM_ADSP_BASE + SMI32_MSM_PMEM_ADSP_SIZE)
+#define SMI32_MSM_FB_SIZE      0x9b000
+
+#define SMI32_MSM_RAM_CONSOLE_BASE  (MSM_EBI_BASE + 0x6d00000)
+#define SMI32_MSM_RAM_CONSOLE_SIZE  128 * SZ_1K
 
 
-#define MSM_PMEM_GPU1_SIZE	0x800000
-#define MSM_PMEM_GPU1_BASE     (MSM_RAM_CONSOLE_BASE + MSM_RAM_CONSOLE_SIZE)
+#define SMI32_MSM_PMEM_GPU1_SIZE  0x800000
+#define SMI32_MSM_PMEM_GPU1_BASE  (SMI32_MSM_RAM_CONSOLE_BASE - SMI32_MSM_PMEM_GPU1_SIZE)
 
-#define MSM_RAM_CONSOLE_BASE	0x169E0000
-#define MSM_RAM_CONSOLE_SIZE	128 * SZ_1K
+#define SMI32_MSM_PMEM_CAMERA_BASE  0
+#define SMI32_MSM_PMEM_CAMERA_SIZE  0
 
-#if (SMI32_MSM_FB_BASE + SMI32_MSM_FB_SIZE) >= (MSM_PMEM_GPU1_BASE)
-#error invalid memory map
-#endif
-
-#if (SMI64_MSM_FB_BASE + SMI64_MSM_FB_SIZE) >= (MSM_PMEM_GPU1_BASE)
-#error invalid memory map
 #endif
 
 #define EBI1_DUAL_128MB_128MB	17